647ab91e2da68c3a3434dea9fa1406e513716703
tontus <tonmoyshaleh@gmail.com>
2020-05-10 16:16:34 +0600

    progress bar added

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("This is Home"),
+      child: new LinearPercentIndicator(
+                width: 140.0,
+                lineHeight: 14.0,
+                percent: 0.5,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+              ),
     );
   }
 }
