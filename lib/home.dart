fb9be97a968074256a31b0f8e61a6962e6fa00dd
Borhan Uddin <borhan.ur@gmail.com>
2020-05-12 14:27:04 +0600

    WIP

--- lib/home.dart
+++ lib/home.dart
@@ -136,7 +136,7 @@ class _HomeState extends State<Home> {
             ],
           );
         } else {
-          return CircularProgressIndicator();
+          return Center(child: CircularProgressIndicator());
         }
       },
     );
